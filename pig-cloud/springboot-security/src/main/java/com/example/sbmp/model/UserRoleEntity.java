48de4420099e4a176018885eb4d19944bb54b1de
hancl <han895268684long@163.com>
2017-09-12 14:29:13 +0800

    ddd

--- pig-cloud/springboot-security/src/main/java/com/example/sbmp/model/UserRoleEntity.java
+++ pig-cloud/springboot-security/src/main/java/com/example/sbmp/model/UserRoleEntity.java
@@ -2,6 +2,10 @@ package com.example.sbmp.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
+/**
+ * 用户的角色
+ *
+ */
 public class UserRoleEntity implements GrantedAuthority{
 	private static final long serialVersionUID = 1L;
 
