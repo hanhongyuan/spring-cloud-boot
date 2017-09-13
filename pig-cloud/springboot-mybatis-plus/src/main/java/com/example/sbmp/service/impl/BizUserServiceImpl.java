ccfc8b3bf00af6643e6b292721782a7858aa4097
hancl <han895268684long@163.com>
2017-09-13 17:50:17 +0800

    TT

--- pig-cloud/springboot-mybatis-plus/src/main/java/com/example/sbmp/service/impl/BizUserServiceImpl.java
+++ pig-cloud/springboot-mybatis-plus/src/main/java/com/example/sbmp/service/impl/BizUserServiceImpl.java
@@ -5,13 +5,15 @@ import com.example.sbmp.mapper.BizUserMapper;
 import com.example.sbmp.service.IBizUserService;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class BizUserServiceImpl extends ServiceImpl<BizUserMapper, BizUser> implements IBizUserService {
-    public List<BizUser> findUserAndScoreById() {
-    	List<BizUser> list = baseMapper.findUserAndScoreById();
-        return list;
-    }
+	@Transactional
+	public List<BizUser> findUserAndScoreById() {
+		List<BizUser> list = baseMapper.findUserAndScoreById();
+		return list;
+	}
 }
