24b40f038f543410f769d1b12c5ff280695994d9
hancl <han895268684long@163.com>
2017-09-14 16:19:45 +0800

    TT

--- pig-cloud/springcloud-service-consumer-ribbon/src/main/java/com/example/sbmp/web/ConsumerController.java
+++ pig-cloud/springcloud-service-consumer-ribbon/src/main/java/com/example/sbmp/web/ConsumerController.java
@@ -9,15 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * <p>
- * 前端控制器
- * </p>
- *
- * @author lengleng
- * @since 2017-08-19
- */
-@SuppressWarnings("unused")
 @RestController
 public class ConsumerController {
     @Autowired
